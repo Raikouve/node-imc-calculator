aa31fe26c4f3335ac7bb9268f4e8f713d350d159
Antônio Gonçalves <tonygoldmarket@yahoo.com>
2022-02-09 10:09:20 -0300

    adicionada função de classificação de IMC

--- imc.js
+++ imc.js
@@ -1,7 +1,35 @@
 const calculateImc = (weight, height) => {
   return (weight / Math.pow(height, 2)).toFixed(2);
-}
+};
+
+const imcClassification = (imcResult) => {
+
+    if (imcResult < 18.5) {
+      return "Magreza";
+    }
+
+    if (imcResult >= 18.5 && imcResult <= 24.9) {
+      return "Normal";
+    }
+
+    if (imcResult >= 25 && imcResult <= 29.9) {
+      return "Sobrepeso";
+    }
+
+    if (imcResult >= 25 && imcResult <= 29.9) {
+      return "Sobrepeso";
+    }
+
+    if (imcResult >= 30 && imcResult <= 39.9) {
+      return "Obesidade";
+    }
+
+    if (imcResult >= 40) {
+      return "Obesidade Grave";
+    }
+};
 
 module.exports = {
-  calculateImc
+  calculateImc,
+  imcClassification,
 };
--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+const fs = require('fs');
+
 const ask = require('./ask');
 const imc = require('./imc');
 
-const name = ask.askName();
-const weight = ask.askWeight();
-const height = ask.askHeight();
-const imcResult = imc.calculateImc(weight, height);
+function main() {
+  const name = ask.askName();
+  const weight = ask.askWeight();
+  const height = ask.askHeight();
+  const imcResult = imc.calculateImc(weight, height);
+  const classification = imc.imcClassification(imcResult);
 
-console.log(`Olá ${name}, seu IMC é ${imcResult}`);
+  fs.appendFileSync('imc.txt', `Olá ${name}, seu IMC é ${imcResult}. Sua classificação de peso é ${classification}.\n`)/
+
+  console.log(`Olá ${name}, seu IMC foi salvo com sucesso!`);
+}
+
+main();
